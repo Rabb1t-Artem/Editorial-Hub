4f1115fb2c13372bda4581eccc6e7275d51f3725
Artem Shatylo <Alukard32244@gmail.com>
2024-12-01 18:57:44 +0200

    add models Redactor Newspaper Topic

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    
+    class Meta:
+        db_table = "redactors"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.username})"
--- news/models.py
+++ news/models.py
@@ -1,3 +1,26 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    publication_date = models.DateField()
+    topic = models.ManyToManyField(
+        Topic,
+        related_name="newspapers",
+    )
+    publishers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="newspapers",
+    )
+
+    def __str__(self):
+        return self.title
